4ea9a6f89bbe0196fec986d559b8b8a572e157db
zhangjiangen <18131652026@163.com>
2016-12-05 08:58:50 +0800

    洗车界面

---
+++ 车嬷嬷Tests/___Tests.m
@@ -0,0 +1,39 @@
+//
+//  ___Tests.m
+//  车嬷嬷Tests
+//
+//  Created by 张建根 on 2016/11/23.
+//  Copyright © 2016年 廊坊市哲涵汽车服务有限公司. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___Tests : XCTestCase
+
+@end
+
+@implementation ___Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
